5e7c17ed2fff33f42cf63650dccbfe451f0e64a4
Yusuf <easyclick06@gmail.com>
2025-03-03 14:10:24 +0100

    * added url shortner route

---
+++ internal/routes/url_routes.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"net/http"
+	"url-shortner/cmd/web"
+	"url-shortner/internal/handler"
+	"url-shortner/internal/repository"
+	"url-shortner/internal/service"
+
+	"github.com/a-h/templ"
+	"gorm.io/gorm"
+)
+
+func RegisterURLRoutes(mux *http.ServeMux, db *gorm.DB) {
+	urlRepo := repository.NewURLRepository(db)
+	urlSvc := service.NewURLService(urlRepo)
+	urlHandler := handler.NewURLHandler(urlSvc)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		switch r.Method {
+		case http.MethodGet:
+			handler := templ.Handler(web.HelloForm())
+			handler.ServeHTTP(w, r)
+		case http.MethodPost:
+			urlHandler.CreateShortURL(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+}
